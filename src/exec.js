a0efb0114a00210d0b88ed9377ec4e7011645988
Patrick Williams <patrickw@porch.com>
2017-01-19 11:00:21 -0800

    include stderr/stdout in error reporting

--- src/exec.js
+++ src/exec.js
@@ -1,14 +1,26 @@
 import Q from 'q';
 import childProcess from 'child_process';
+import VError from 'verror';
 
 export default (cmd, { logger, ...options } = {}) => {
     const defer = Q.defer();
     logger.trace(`exec ${cmd}`);
     childProcess.exec(cmd, {
         ...options
-    }, defer.makeNodeResolver());
-    return defer.promise.spread((stdout, stderr) => {
+    }, (err, stdout, stderr) => {
         logger.trace({ stdout, stderr });
-        return stdout;
+
+        if (err) {
+            defer.reject(new VError({
+                cause: err,
+                info: {
+                    stdout,
+                    stderr
+                }
+            }));
+        } else {
+            defer.resolve(stdout);
+        }
     });
+    return defer.promise;
 };
