7acd88be5a7fb960e5ac47a914ac2723ebf91294
Patrick Williams <pwmckenna@gmail.com>
2017-04-06 11:57:17 -0700

    bail on updates when the latest version of a dep matches existing semver

--- src/pkg.js
+++ src/pkg.js
@@ -4,6 +4,7 @@ import Q from 'q';
 import exec from './exec';
 import semverRegex from 'semver-regex';
 import { memoize } from 'lodash';
+import { satisfies } from 'semver';
 
 // update to exact versions
 // return whether the new version is not a major bump (ie, safe to merge without review)
@@ -16,32 +17,29 @@ const getPublishedVersion = memoize((packageName, logger) => (
     exec(`npm view ${packageName} version`, { logger }).then(version => version.trim())
 ));
 
-const getVersion = ({ path, packageName, logger }) => (
+const getVersion = ({ path, packageName }) => (
     Q.fcall(() => (
         jsonFile(path)
     )).then(file => (
-        Q.all([
-            file.get(),
-            getPublishedVersion(packageName, logger)
-        ]).spread(({
-            dependencies = {},
-            devDependencies = {},
-            peerDependencies = {}
-        }) => {
-            if (dependencies.hasOwnProperty(packageName)) {
-                assert(!devDependencies.hasOwnProperty(packageName), `${packageName} found in both dependencies and devDependencies`);
-                assert(!peerDependencies.hasOwnProperty(packageName), `${packageName} found in both dependencies and peerDependencies`);
-                return exactVersion(dependencies[packageName]);
-            }
-            if (devDependencies.hasOwnProperty(packageName)) {
-                return exactVersion(devDependencies[packageName]);
-            }
-            if (peerDependencies.hasOwnProperty(packageName)) {
-                return exactVersion(peerDependencies[packageName]);
-            }
-            throw new Error(`${packageName} not found`);
-        })
-    ))
+        file.get()
+    )).then(({
+        dependencies = {},
+        devDependencies = {},
+        peerDependencies = {}
+    }) => {
+        if (dependencies.hasOwnProperty(packageName)) {
+            assert(!devDependencies.hasOwnProperty(packageName), `${packageName} found in both dependencies and devDependencies`);
+            assert(!peerDependencies.hasOwnProperty(packageName), `${packageName} found in both dependencies and peerDependencies`);
+            return exactVersion(dependencies[packageName]);
+        }
+        if (devDependencies.hasOwnProperty(packageName)) {
+            return exactVersion(devDependencies[packageName]);
+        }
+        if (peerDependencies.hasOwnProperty(packageName)) {
+            return exactVersion(peerDependencies[packageName]);
+        }
+        throw new Error(`${packageName} not found`);
+    })
 );
 
 const updateVersion = ({ path, packageName, logger }) => (
@@ -57,6 +55,7 @@ const updateVersion = ({ path, packageName, logger }) => (
             peerDependencies = {}
         }, version) => {
             if (dependencies.hasOwnProperty(packageName)) {
+                assert(!satisfies(version, dependencies[packageName]), `${packageName} latest version ${version} matches existing dependency ${dependencies[packageName]}`);
                 return file.set({
                     dependencies: {
                         [packageName]: `^${version}`
@@ -64,6 +63,7 @@ const updateVersion = ({ path, packageName, logger }) => (
                 });
             }
             if (devDependencies.hasOwnProperty(packageName)) {
+                assert(!satisfies(version, devDependencies[packageName]), `${packageName} latest version ${version} matches existing devDependency version ${devDependencies[packageName]}`);
                 return file.set({
                     devDependencies: {
                         [packageName]: `^${version}`
@@ -71,6 +71,7 @@ const updateVersion = ({ path, packageName, logger }) => (
                 });
             }
             if (peerDependencies.hasOwnProperty(packageName)) {
+                assert(!satisfies(version, peerDependencies[packageName]), `${packageName} latest version ${version} matches existing peerDependency version ${peerDependencies[packageName]}`);
                 return file.set({
                     peerDependencies: {
                         [packageName]: `^${version}`
